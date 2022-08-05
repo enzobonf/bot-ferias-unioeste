2a0fd112e8f71f91003c2a150a588bfa6916d97a
enzobonf <enzobonfx@gmail.com>
2022-08-05 00:36:18 -0300

    alterar config heroku

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ const { APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_SECRET, DATA_FERIAS } = proces
 const inicioFerias = new Date(DATA_FERIAS);
 inicioFerias.setUTCHours(3,0,0,0);
 
+console.log(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_SECRET, DATA_FERIAS);
+
 console.log('Bot iniciado com sucesso!');
 
 async function executaTweet(){
